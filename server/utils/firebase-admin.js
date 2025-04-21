396cfd23171ab7cec5b2c0d5cb959e88d49fe614
Swayam Bhalotia <swayambhalotia@gmail.com>
2025-04-21 19:22:14 +0530

    Update firebase-admin.js

--- server/utils/firebase-admin.js
+++ server/utils/firebase-admin.js
@@ -4,7 +4,11 @@ const serviceAccount = require("./service-account.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+     credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
   });
   console.log("Firebase Admin initialized successfully.",admin);
 }
